4d9aec2fb17673fffa07895537723c76224eb5ac
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-12 13:14:01 +0530

    Initial commit
    grunt-contrib-watch — Run tasks whenever watched files change.
    Usage: $ npm install grunt-contrib-watch --save-dev | Help: https://github.com/gruntjs/grunt-contrib-watch

---
+++ tasks/options/watch.js
@@ -0,0 +1,30 @@
+/*!
+ * ----------
+ * FRDB®-Data
+ * ----------
+ * Data of FRDB — A manually curated dedicated free radicals database.
+ * ___________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 11-Dec-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-watch
+ * @build     : SEED™ — Örebro
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "FRDB®-Data".
+ * ___________________________________________________________________________
+ */
